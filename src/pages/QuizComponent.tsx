3f853b706911af3363455067adbb4ec3479f24ce
Hasoyama <pickkllee32342@gmail.com>
2024-05-24 18:39:24 +0300

    add timer

--- src/pages/QuizComponent.tsx
+++ src/pages/QuizComponent.tsx
@@ -7,13 +7,16 @@ import { useParams } from 'react-router-dom';
 import { Quiz } from '../types';
 
 const QuizComponent = () => {
+    const { id } = useParams<{ id: string }>()
+    const { quizzes } = useSelector((state: State) => state.quizzes)
+
     const [questionId, setQuestionId] = useState(0)
     const [answered, setAnswered] = useState(false)
     const [answeredArr, setAnsweredArr] = useState<boolean[]>([])
     const [quiz, setQuiz] = useState<Quiz | undefined>(undefined)
+    const [timer, setTimer] = useState(0)
+    const [quizCompleted, setQuizCompleted] = useState(false)
 
-    const { id } = useParams<{ id: string }>()
-    const { quizzes } = useSelector((state: State) => state.quizzes)
     useEffect(() => {
         const fetchQuiz = async () => {
             const foundQuiz = await quizzes.find((quiz: Quiz) => quiz.id === id)
@@ -22,11 +25,24 @@ const QuizComponent = () => {
         fetchQuiz();
     }, [id, quizzes])
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimer(prevTimer => prevTimer + 1)
+        }, 1000)
+
+        if (quizCompleted) clearInterval(interval)
+
+        return () => clearInterval(interval)
+
+    }, [quizCompleted])
+
     if (!quiz) {
         return <div>Loading...</div>
     }
 
     const questions = quiz.questions
+
+
 
     const nextQuestion = () => {
         if (questionId !== questions.length - 1 && answered) {
@@ -35,8 +51,9 @@ const QuizComponent = () => {
             resetAnswerStyles()
         }
         if (questionId === questions.length - 1) {
+            setQuizCompleted(true)
             const correctAnswers = answeredArr.filter(answer => answer).length;
-            alert(`Congratulations, you answered ${correctAnswers} out of ${questions.length} correctly!`)
+            alert(`Congratulations, you answered ${correctAnswers} out of ${questions.length} correctly! It took you ${timer} seconds.`)
         }
 
     }
@@ -92,7 +109,10 @@ const QuizComponent = () => {
                             )
                         }
                     </div>
-                    <button className="quiz-button" onClick={nextQuestion}>Next</button>
+                    <div className="flex items-center justify-between">
+                        <button className="quiz-button" onClick={nextQuestion}>Next</button>
+                        <div className="quiz-timer">Time Elapsed: {timer} seconds</div>
+                    </div>
                 </div>
             </div>
         </div>
